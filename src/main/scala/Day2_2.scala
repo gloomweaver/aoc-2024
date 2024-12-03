b1d6bdb756745c02e05e3e49ba9e0804b5e2e86a
Kirill Khramkov <hramkir@gmail.com>
2024-12-03 04:35:12 +0400

    Day 2

---
+++ src/main/scala/Day2_2.scala
@@ -0,0 +1,45 @@
+import scala.jdk.StreamConverters._
+
+object Day2_2 extends App {
+  def isSafe(values: Array[Int]): Boolean = {
+    val compare = values.take(2) match {
+      case Array(a, b) if a < b => (x: Int, y: Int) => x < y
+      case Array(a, b) if a > b => (x: Int, y: Int) => x > y
+      case _                    => return false
+    }
+
+    values
+      .sliding(2)
+      .map { case Array(a, b) =>
+        val diff = Math.abs(a - b)
+
+        diff >= 1 && diff <= 3 && compare(a, b)
+      }
+      .forall(identity)
+  }
+
+  def canBeSafe(values: Array[Int]): Boolean = {
+    if (isSafe(values)) return true
+
+    values.indices.exists { index =>
+      isSafe(values.take(index) ++ values.drop(index + 1))
+    }
+  }
+
+  val input = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+
+  val res = input
+    .lines()
+    .toScala(List)
+    .map(_.trim.split("\\s+").map(_.toInt))
+    .map(canBeSafe)
+    .filter(identity)
+    .length
+
+  println(res)
+}
